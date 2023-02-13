e5350868e3abf1b456c660959bbef85e75c326c7
seiya-lab <scb01088@edu.osakafu-u.ac.jp>
2023-02-13 13:09:45 +0900

    feat: 素数判定プログラム

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 # 室屋がPythonを学ぶファイル
 
-# TODO "HELLO WORLD!"と出力されるプログラムを書いてください。
+# TODO 素数を判定するプログラムを作成してください。
+# 引数が正の整数で、出力がbooleanで返してください
+def isPrime(num):
+    # ここを変更してください
+    return True
+
+for num in range(2, 100):
+    if isPrime(num):
+        print(str(num) + "は素数です。文句あるか")
