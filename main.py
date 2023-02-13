8df47bd808a2339a675aac49c3cc2bd49f59e5c4
seiya-lab <72550275+seiya-lab@users.noreply.github.com>
2023-02-13 16:00:59 +0900

    feat:九九

--- main.py
+++ main.py
@@ -1,26 +1,2 @@
 # 室屋がPythonを学ぶファイル
-# TODO 回文を判定するプログラムを作成してください。
-def is_kaibun(text):
-    reverced_text = text[::-1] #文字列のスライス(逆から読む)
-    if reverced_text == text:
-      return True
-
-
-texts = ["たけやぶやけた", "a", "わたしはたわし"]
-
-for text in texts:
-    if is_kaibun(text):
-        print(text, "は回文ですよ")
-    else:
-        print(text,"は回文じゃないです！！")
-
-#もしスライスを使わない方法だったら、最初の文字と最後の文字を比較していく方法もある
-# def is_kaibun(text):
-#     left = 0
-#     right = len(text) - 1
-#     while left < right:
-#         if text[left] != text[right]:
-#             return False
-#         left += 1
-#         right -= 1
-#     return True
+# TODO 掛け算の九九を9×9の二次元配列の中に格納するプログラムを書いてください。
