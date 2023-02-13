76ccdfa93a371e133ba7b3f91b93b60e4fb90534
seiya-lab <scb01088@edu.osakafu-u.ac.jp>
2023-02-13 15:57:48 +0900

    feat: 九九

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
 # 室屋がPythonを学ぶファイル
-# TODO 回文を判定するプログラムを作成してください。
-# 引数は文字列、出力はブール値
-def is_kaibun(str1):
-    return True
-
-texts = ["たけやぶやけた", "a", "わたしはたわし"]
-
-for text in texts:
-    if is_kaibun:
-        print(text, "は回文ですよ")
+# TODO 掛け算の九九を9×9の二次元配列の中に格納するプログラムを書いてください。
