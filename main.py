1e8707c90d7d745eba220a0e6d15bdf4f72724a7
keita324 <tempura0324@gmail.com>
2023-02-13 15:48:13 +0900

    回文

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 # 室屋がPythonを学ぶファイル
-# TODO 素数を判定するプログラムを作成してください。
-# 引数が正の整数で、出力がbooleanで返してください
-
-from re import I
-
-#数字が素数かどうか判断
-def isPrime(num):
-    for i in range(2,num):
-      if num % i == 0:
-        return False
-    return True
-
-#2から100までの数字を列挙するforループ
-for i in range(2, 100):
-    if isPrime(i):
-        print(str(i) + "は素数です。文句あるか")
+# TODO 回文を判定するプログラムを作成してください。
+# 引数は文字列、出力はブール値
 
 
-# def isPrime(num): もし5が入ったら
-#     for i in range(2,num): 2-4までの数字を繰り返す(2,3,4) rangeはnum-1となるため
-#       if num % i == 0: 5が4以下の数字(1は除く)で割り切れたら
-#         return False 動作終わり
-#     return True そうじゃなかったらTRUEで返す
+def is_kaibun(text):
+    reverced_text = text[::-1] #文字列のスライス(逆から読む)
+    if reverced_text == text:
+      return True
+
+
+texts = ["たけやぶやけた", "a", "わたしはたわし"]
+
+for text in texts:
+    if is_kaibun(text):
+        print(text, "は回文ですよ")
+    else:
+        print(text,"は回文じゃないです！！")
+
+#もしスライスを使わない方法だったら、最初の文字と最後の文字を比較していく方法もある
+# def is_kaibun(text):
+#     left = 0
+#     right = len(text) - 1
+#     while left < right:
+#         if text[left] != text[right]:
+#             return False
+#         left += 1
+#         right -= 1
+#     return True
